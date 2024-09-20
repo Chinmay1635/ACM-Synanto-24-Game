e1e3060f7070fbe3efd93bec41536492cce03c0d
Sharaneshwar <punjalsharaneshwar@gmail.com>
2024-09-20 11:18:13 +0530

    added backend verification route

--- app.js
+++ app.js
@@ -94,5 +94,9 @@ app.get('/api/user/leaderboard', async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the game server!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
